f87a8567b2c9f6223d888c49e2922bd884536181
raghad-alakhras <ragadayman5@gmail.com>
2026-04-26 16:48:03 -0700

    edit token in production

--- src/utilties/getTokenFun.ts
+++ src/utilties/getTokenFun.ts
@@ -4,7 +4,7 @@ import { cookies } from "next/headers";
 export async function getTokenFun(){
     const cookie = await cookies()
     const cookieName = process.env.NODE_ENV==='production'
-    ?'__Secure-next-autj.session-token'
+    ?'__Secure-next-auth.session-token'
     :'next-auth.session-token'
     const nextAuthToken = cookie.get(cookieName)?.value
     const decodedCookie= await decode({
